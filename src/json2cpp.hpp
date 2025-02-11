9366d063263b7f50a9cf1bfeb151237097fb7b45
reg31 <37986256+reg31@users.noreply.github.com>
2025-02-11 23:46:20 +0000

    minor indentation fix

--- src/json2cpp.hpp
+++ src/json2cpp.hpp
@@ -37,19 +37,13 @@ struct compile_results
   std::vector<std::string> impl;
 };
 
-
 std::string compile(const nlohmann::json &value, std::size_t &obj_count, std::vector<std::string> &lines);
-
-
 compile_results compile(const std::string_view document_name, const nlohmann::json &json);
-
 compile_results compile(const std::string_view document_name, const std::filesystem::path &filename);
-
 
 void write_compilation(std::string_view document_name,
   const compile_results &results,
   const std::filesystem::path &base_output);
-
 
 void compile_to(const std::string_view document_name,
   const nlohmann::json &json,
@@ -58,6 +52,5 @@ void compile_to(const std::string_view document_name,
 void compile_to(const std::string_view document_name,
   const std::filesystem::path &filename,
   const std::filesystem::path &base_output);
-
 
 #endif
