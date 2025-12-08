082731b5975c127e1cc6300f29e9592ca0442658
reg31 <37986256+reg31@users.noreply.github.com>
2025-12-08 13:26:27 +0000

    Update dependency versions in Dependencies.cmake

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -9,7 +9,7 @@ function(json2cpp_setup_dependencies)
   # already been provided to us by a parent project
 
   if(NOT TARGET fmtlib::fmtlib)
-    cpmaddpackage("gh:fmtlib/fmt#11.2.0")
+    cpmaddpackage("gh:fmtlib/fmt#12.1.0")
   endif()
 
   if(NOT TARGET spdlog::spdlog)
@@ -17,7 +17,7 @@ function(json2cpp_setup_dependencies)
       NAME
       spdlog
       VERSION
-      1.15.3
+      1.16.0
       GITHUB_REPOSITORY
       "gabime/spdlog"
       OPTIONS
@@ -25,11 +25,11 @@ function(json2cpp_setup_dependencies)
   endif()
 
   if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.8.1")
+    cpmaddpackage("gh:catchorg/Catch2@3.11.0")
   endif()
 
   if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage("gh:CLIUtils/CLI11@2.5.0")
+    cpmaddpackage("gh:CLIUtils/CLI11@2.6.1")
   endif()
 
   if(NOT TARGET ftxui::screen)
