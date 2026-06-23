8c0b85df504d8d58551a00f6a86f8d0a7850b5cf
reg31 <37986256+reg31@users.noreply.github.com>
2026-06-23 22:29:49 +0100

    Update Dependencies.cmake

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -9,7 +9,7 @@ function(json2cpp_setup_dependencies)
   # already been provided to us by a parent project
 
   if(NOT TARGET fmtlib::fmtlib)
-    cpmaddpackage("gh:fmtlib/fmt#12.1.0")
+    cpmaddpackage("gh:fmtlib/fmt#12.2.0")
   endif()
 
   if(NOT TARGET spdlog::spdlog)
@@ -25,7 +25,7 @@ function(json2cpp_setup_dependencies)
   endif()
 
   if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.15.0")
+    cpmaddpackage("gh:catchorg/Catch2@3.15.1")
   endif()
 
   if(NOT TARGET CLI11::CLI11)
@@ -33,7 +33,7 @@ function(json2cpp_setup_dependencies)
   endif()
 
   if(NOT TARGET ftxui::screen)
-    cpmaddpackage("gh:ArthurSonzogni/FTXUI@6.1.9")
+    cpmaddpackage("gh:ArthurSonzogni/FTXUI@7.0.0")
   endif()
 
   if(NOT TARGET nlohmann_json::nlohmann_json)
